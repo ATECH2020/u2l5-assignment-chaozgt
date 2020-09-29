0a4a58de200b423ee2f0071a79f3ddc1dd0758f9
Ismael Benhayoune <ismael.225781@nv.ccsd.net>
2020-09-29 22:15:57 +0000

    I made it so that it uses the methods in the other 2 classes to print out the properly needed information.

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,18 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+      
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter the sales tax rate:");
+      double sTR = scanner.nextDouble(); //sales tax rate
+      System.out.println("How many boards do you need?");
+      int boards = scanner.nextInt();
+      System.out.println("How many windows do you need?");
+      int windows = scanner.nextInt();
+      Construction Construct = new Construction(8.00, 11.00, sTR);
+      double total = Construct.lumberCost(boards) + Construct.windowCost(windows);
+      System.out.println("Total: " + total);//prints the total amount
+      System.out.print("Grand Total: ");
+      System.out.print(Construct.grandTotal(total));
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,17 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the latitude of the starting location: ");
+        double lat1 = scanner.nextDouble();
+        System.out.print("Enter the longitude of the starting location: ");
+        double long1 = scanner.nextDouble();
+        System.out.print("Enter the latitude of the ending location: ");
+        double lat2 = scanner.nextDouble();
+        System.out.print("Enter the longitude of the ending location: ");
+        double long2 = scanner.nextDouble();
+        GeoLocation gL = new GeoLocation(lat1, long1); // initializes other class first time
+        GeoLocation gL2 = new GeoLocation(lat2, long2); // initializes other class second time
+        System.out.print("The distance is " + gL.distanceFrom(gL2) + " miles.");
     }
 }
